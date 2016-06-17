230cba827ad9b7cc3cebec5be3415a66ce1ee2df
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2016-06-17 18:55:57 +0200

    updated title

--- conf.py
+++ conf.py
@@ -146,7 +146,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = u'x64dbg v0.1'
+html_title = u'x64dbg documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
