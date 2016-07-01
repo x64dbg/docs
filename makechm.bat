10a1e8fa6cb7f2c30387cad174653bf5e76a536d
Torusrxxx <routeping@gmail.com>
2016-07-01 14:26:07 +0000

    batch for creating CHM

---
+++ makechm.bat
@@ -0,0 +1,10 @@
+@echo off
+if "%VSVARSALLPATH%"=="" set VSVARSALLPATH=c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
+echo Setting VS Path
+call "%VSVARSALLPATH%"
+echo Building Help Project
+call make htmlhelp
+echo Building CHM File
+hhc .\_build\htmlhelp\x64dbgdoc.hhp
+copy .\_build\htmlhelp\x64dbgdoc.chm x64dbg.chm
+echo Finished
