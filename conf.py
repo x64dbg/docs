147537c2a524bf535a737bc08a5e56328958bc5b
mrexodia <mr.exodia.tpodt@gmail.com>
2017-08-01 21:50:27 +0200

    hacks for better CHM display

--- conf.py
+++ conf.py
@@ -209,7 +209,7 @@ html_static_path = ['_static']
 
 # If true, links to the reST sources are added to the pages.
 #
-# html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #
--- makechm.bat
+++ makechm.bat
@@ -4,7 +4,9 @@ echo Setting VS Path
 call "%VSVARSALLPATH%"
 echo Building Help Project
 call make htmlhelp
+echo Applying CHM hacks
 copy theme.js .\_build\htmlhelp\_static\js\theme.js
+type hacks.css >> .\_build\htmlhelp\_static\css\theme.css
 echo Building CHM File
 hhc .\_build\htmlhelp\x64dbgdoc.hhp
 copy .\_build\htmlhelp\x64dbgdoc.chm x64dbg.chm
